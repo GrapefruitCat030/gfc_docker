70c3ed18c93e50c90fed64bea5ad94b2b51223d3
GrapefruitCat030 <100248345+GrapefruitCat030@users.noreply.github.com.>
2024-12-22 17:15:35 +0800

    feature: change commit command

--- cmd/commit.go
+++ cmd/commit.go
@@ -1,26 +1,29 @@
 package cmd
 
 import (
+	"fmt"
 	"os/exec"
+	"path/filepath"
 
+	gfc_runinfo "github.com/GrapefruitCat030/gfc_docker/pkg/runinfo"
 	"github.com/spf13/cobra"
 )
 
 func init() {
-	commitCmd.Flags().StringVarP(&commitConf.containerRootPath, "dir", "d", "/root/project/gfc_docker/filesystem/merged", "container root path")
 	rootCmd.AddCommand(commitCmd)
 }
 
 type CommitConfig struct {
-	containerRootPath string
+	containerName string
+	imageName     string
 }
 
 var commitConf CommitConfig
 
 var commitCmd = &cobra.Command{
-	Use:   "commit [container-name]",
+	Use:   "commit [container-name] [image-name]",
 	Short: "Commit a container into an image",
-	Args:  cobra.MinimumNArgs(1),
+	Args:  cobra.MinimumNArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
 		commit(args)
 	},
@@ -28,8 +31,16 @@ var commitCmd = &cobra.Command{
 
 func commit(args []string) {
 	containerName := args[0]
-	imageTar := containerName + ".tar"
-	if _, err := exec.Command("tar", "-czf", imageTar, "-C", commitConf.containerRootPath, ".").CombinedOutput(); err != nil {
-		panic(err)
+	imageName := args[1]
+	imageTar := imageName + ".tar"
+	cinfo, err := gfc_runinfo.GetContainerInfo(containerName)
+	if err != nil {
+		fmt.Println("Error getting container info - ", err)
+		return
+	}
+	path := filepath.Join(cinfo.RootFs, runConf.UFSer.WorkSpace())
+	if _, err := exec.Command("tar", "-czf", imageTar, "-C", path, ".").CombinedOutput(); err != nil {
+		fmt.Println("Error creating image tar - ", err)
+		return
 	}
 }
